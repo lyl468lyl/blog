3175be57e4ec79f77a96a90d26e9f51e93dcfb6f
qyc <759160301@qq.com>
2022-01-24 18:15:12 +0800

    更新配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,4 +5,12 @@ export default defineUserConfig<DefaultThemeOptions>({
   base: "/blog/",
   lang: "zh-CN",
   title: "学习记录",
+  themeConfig: {
+    home: "/",
+    navbar: [{ text: "日志记录", link: "/" }],
+    repo: "caigouzi1/blog",
+    contributors: false,
+    editLink: false,
+    lastUpdatedText: "更新时间",
+  },
 });
